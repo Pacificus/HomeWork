6d27c1104680847e22f9a30311dd10823725aad1
Dima <greshchook@gmail.com>
2017-11-03 15:52:43 +0200

    Program find a max of three int used method with two parrams

---
+++ MaxOf3Num/src/com/hillel/Task.java
@@ -0,0 +1,20 @@
+package com.hillel;
+
+public class Task{
+	public static void main(String...args){
+		if (args.length < 3){
+			System.out.println("Give me more paramms...or input nums");
+			return;
+		}
+		
+		int first = Integer.parseInt(args[0]), second = Integer.parseInt(args[1]), third = Integer.parseInt(args[2]);
+		Task task = new Task();
+		int max = task.MaxOfTwoNum(first, second);
+		max = task.MaxOfTwoNum(max, third);
+		System.out.println("Max of " + first + " " + second + " " + third + " is:\t" +max);
+	}
+	
+	private int MaxOfTwoNum(int num1, int num2){
+		return (num1 > num2) ? num1 : num2;
+	}
+}
